45566996a158f9e644b15abe07110a8f2ffd91d9
larsvandriel <lars.van.driel@hotmail.com>
2022-05-30 13:48:24 +0200

    Implemented api

---
+++ InventoryManagementSystem/ims_api/Dtos/DecreaseStockEventDto.cs
@@ -0,0 +1,9 @@
+﻿namespace InventoryManagementSystem.API.Dtos
+{
+    public class DecreaseStockEventDto
+    {
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Amount { get; set; }
+    }
+}
--- InventoryManagementSystem/ims_api/Program.cs
+++ InventoryManagementSystem/ims_api/Program.cs
@@ -1,6 +1,38 @@
+using InventoryManagementSystem.API.RabbitMQ;
+using InventoryManagementSystem.DataAccessLayer;
+using InventoryManagementSystem.Logic;
+using InventoryManagementSystem.RabbitMQAccessLayer;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+builder.Services.Configure<IISOptions>(options =>
+{
+
+});
+
+ConfigurationManager config = builder.Configuration;
+var connectionString = config["mssqlconnection:connectionString"];
+
+builder.Services.AddDbContext<RepositoryContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddHostedService<MessageBusSubscriberDecreaseStockEvent>();
+builder.Services.AddHostedService<MessageBusSubscriberProductCreatedEvent>();
+builder.Services.AddHostedService<MessageBusSubscriberProductDeletedEvent>();
+builder.Services.AddHostedService<MessageBusSubscriberProductUpdatedEvent>();
+
+builder.Services.AddScoped<IInventoryManager, InventoryManager>();
+builder.Services.AddScoped<IInventoryRepository, InventoryRepository>();
+builder.Services.AddScoped<IMessageBusClient, MessageBusClient>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +47,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions()
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseRouting();
+
+app.UseCors("CorsPolicy");
+
 
 app.UseHttpsRedirection();
 
