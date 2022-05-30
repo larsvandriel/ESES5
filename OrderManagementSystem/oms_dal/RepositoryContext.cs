3e7601b04e7acacc549dea035520a363f2edb7ac
larsvandriel <lars.van.driel@hotmail.com>
2022-05-30 15:59:32 +0200

    Implemented RabbitMQ Access Layer

---
+++ OrderManagementSystem/oms_dal/RepositoryContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManagementSystem.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.DataAccessLayer
+{
+    public class RepositoryContext: DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+        }
+    }
+}
